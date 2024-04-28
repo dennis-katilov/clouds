95e9569c44b2fc961248a0ca4d6bede95f6d7af5
katilov <katilov.denis@gmail.com>
2024-04-28 18:29:17 +0300

    middleware authRouter

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
+const authRouter = require('./routes/auth.routes')
 
 const app = express()
 const PORT = config.get('serverPORT')
+
+app.use("/api/auth", authRouter)
+
 const start = async () =>{
     try {
         await mongoose.connect(config.get('db'))
