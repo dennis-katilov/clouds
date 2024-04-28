2996079a2f768b7f085e72295e8442f3c4f88a75
katilov <katilov.denis@gmail.com>
2024-04-28 17:13:23 +0300

    server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log('hello server, and world')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPORT')
+const start = () =>{
+    try {
+        app.listen(PORT, ()=>{
+            console.log('Server start on port', PORT)
+        })
+    } catch (error) {
+        
+    }
+}
+
+start()
