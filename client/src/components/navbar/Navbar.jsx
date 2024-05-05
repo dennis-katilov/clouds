a2d40cde40b53cd9965397423d1458178e60f754
katilov <dennis.katilov@gmail.com>
2024-05-05 20:36:56 +0300

    fix

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import Logo from '../../assets/img/navbar-logo.svg'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <img src='{Logo}' alt="" className="navbar__logo" />
+            <img src={Logo} alt="" className="navbar__logo" />
             <div className="navbar__header">CLOUD</div>
             <div className="navbar__login">Sign In</div>
             <div className="navbar__registration">Sign Up</div>
