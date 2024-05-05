5c2484fd3d0847b6ae04c581bdb3c380df946994
katilov <dennis.katilov@gmail.com>
2024-05-05 19:56:40 +0300

    navbar

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import './navbar.css'
+import Logo from '../../assets/img/navbar__logo.svg'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <img src='{Logo}' alt="" className="navbar__logo" />
+            <div className="navbar__header">CLOUD</div>
+            <div className="navbar__login">Sign In</div>
+            <div className="navbar__registration">Sign Up</div>
+        </div>
+    )
+}
+
+
+export default Navbar
